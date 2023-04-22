99b6523690210f0bff5dc9af5b3053e750673814
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-04-22 17:02:26 -0300

    feat: added email confirmation route

--- app/navigation/stacks/Unsigned.tsx
+++ app/navigation/stacks/Unsigned.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { ForgotPassword, SignIn, SignUp } from '~/domains/auth';
+import {
+  ForgotPassword,
+  SignIn,
+  SignUp,
+  EmailConfirmation,
+} from '~/domains/auth';
 
 export type UnsignedStackParamList = {
   SignIn: undefined;
   SignUp: undefined;
   ForgotPassword: undefined;
+  EmailConfirmation: { email: string };
 };
 
 const Stack = createNativeStackNavigator<UnsignedStackParamList>();
@@ -18,6 +24,7 @@ const Unsigned = (
     <Stack.Screen name="SignIn" component={SignIn} />
     <Stack.Screen name="SignUp" component={SignUp} />
     <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+    <Stack.Screen name="EmailConfirmation" component={EmailConfirmation} />
   </Stack.Group>
 );
 
