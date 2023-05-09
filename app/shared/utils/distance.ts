7f4fd0f8e1b134ee9a3a828b334dbb1b688ca5ea
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-05-09 10:32:45 -0300

    feat: created distance utils to calculate and format distance

---
+++ app/shared/utils/distance.ts
@@ -0,0 +1,28 @@
+import { Location } from '../types';
+
+export function formatDistance(distance: number) {
+  if (distance > 1) {
+    return `${distance.toFixed(1)} km`;
+  }
+
+  return `${distance.toFixed(2)} m`;
+}
+
+export function calcDistance(userLocation: Location, plantLocation: Location) {
+  var R = 6371; // km
+  var dLat = toRad(plantLocation.coordinates[0] - userLocation.coordinates[0]);
+  var dLon = toRad(plantLocation.coordinates[1] - userLocation.coordinates[1]);
+  var lat1 = toRad(userLocation.coordinates[0]);
+  var lat2 = toRad(plantLocation.coordinates[0]);
+
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+}
+
+function toRad(value: number) {
+  return (value * Math.PI) / 180;
+}
