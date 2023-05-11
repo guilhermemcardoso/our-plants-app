b64addeaa201302a2c4aaea53077a4126428187a
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-05-11 09:36:23 -0300

    feat: added new screens to navigation

--- app/navigation/stacks/signed-in.tsx
+++ app/navigation/stacks/signed-in.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import TabNavigator from '~/navigation/tabs';
 import { Routes } from '../routes';
 import { EditProfile } from '~/domains/profile';
+import { useGetSpecies } from '~/hooks/use-get-species';
+import { Plant } from '~/shared/types';
+import { CreateEditPlant, VisualizePlant } from '~/domains/plant';
 
 const screenOptions = { headerShown: false };
 
@@ -14,15 +17,35 @@ export type SignedInStackParamList = {
   [Routes.FAVORITES]: undefined;
   [Routes.USER_PROFILE]: undefined;
   [Routes.SETTINGS]: undefined;
+  [Routes.CREATE_EDIT_PLANT]: {
+    plant?: Plant;
+  };
+  [Routes.VISUALIZE_PLANT]: {
+    plant: Plant;
+  };
 };
 
 const RootStack = createNativeStackNavigator<SignedInStackParamList>();
 
 const Signed = () => {
+  const { getSpecies } = useGetSpecies();
+
+  useEffect(() => {
+    getSpecies();
+  }, [getSpecies]);
+
   return (
     <RootStack.Navigator screenOptions={screenOptions}>
       <RootStack.Screen name={Routes.HOME} component={TabNavigator} />
       <RootStack.Screen name={Routes.EDIT_PROFILE} component={EditProfile} />
+      <RootStack.Screen
+        name={Routes.CREATE_EDIT_PLANT}
+        component={CreateEditPlant}
+      />
+      <RootStack.Screen
+        name={Routes.VISUALIZE_PLANT}
+        component={VisualizePlant}
+      />
     </RootStack.Navigator>
   );
 };
