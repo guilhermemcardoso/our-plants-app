1f03a53b32812e624b2c8c36ed1b065219e15ab0
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-07-23 10:20:59 -0300

    fix: fixed refresh token endpoint

--- app/services/api/index.ts
+++ app/services/api/index.ts
@@ -19,7 +19,7 @@ export async function fetchRefreshToken() {
 
   return Api({
     method: 'post',
-    url: '/auth/refresh',
+    url: '/auth/refresh-token',
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
