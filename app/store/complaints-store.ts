2f92a5aa9e14b3f6d2f8cda4b78f255ddc945401
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-08-04 09:54:28 -0300

    feat: created complaints store

--- app/store/complaints-store.ts
+++ app/store/complaints-store.ts
@@ -3,15 +3,60 @@ import { Complaint } from '~/shared/types';
 
 interface ComplaintsState {
   complaints: Complaint[];
+  complaintsPage: number;
+  hasNextComplaints: boolean;
   setComplaints: (complaints: Complaint[]) => void;
+  setComplaintsPage: (complaintsPage: number) => void;
+  setHasNextComplaints: (hasNext: boolean) => void;
+  myComplaints: Complaint[];
+  myComplaintsPage: number;
+  hasNextMyComplaints: boolean;
+  setMyComplaints: (complaints: Complaint[]) => void;
+  setMyComplaintsPage: (myComplaintsPage: number) => void;
+  setHasNextMyComplaints: (hasNext: boolean) => void;
 }
 
 export const useComplaintsStore = create<ComplaintsState>((set) => ({
   complaints: [],
+  complaintsPage: 1,
+  hasNextComplaints: false,
+  myComplaints: [],
+  myComplaintsPage: 1,
+  hasNextMyComplaints: false,
   setComplaints: async (complaints) => {
     set((state) => ({
       ...state,
       complaints: complaints,
+    }));
+  },
+  setComplaintsPage: async (complaintsPage) => {
+    set((state) => ({
+      ...state,
+      complaintsPage: complaintsPage,
+    }));
+  },
+  setHasNextComplaints: async (hasNext) => {
+    set((state) => ({
+      ...state,
+      hasNextComplaints: hasNext,
+    }));
+  },
+  setMyComplaints: async (complaints) => {
+    set((state) => ({
+      ...state,
+      myComplaints: complaints,
+    }));
+  },
+  setMyComplaintsPage: async (myComplaintsPage) => {
+    set((state) => ({
+      ...state,
+      myComplaintsPage: myComplaintsPage,
+    }));
+  },
+  setHasNextMyComplaints: async (hasNext) => {
+    set((state) => ({
+      ...state,
+      hasNextMyComplaints: hasNext,
     }));
   },
 }));
